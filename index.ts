d7026e2ff3e304e299967fb5dbbb48dacf70b8f3
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-20 23:09:30 +0100

    saved index file

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import dbConfig from "./config/db";
 
-const PORT = 2020;
+const PORT = process.env.PORT || 2023;
 
 const app: Application = express();
-
+dbConfig();
 app.use(express.json());
 app.use(cors());
+
+//default get
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: "api is available for consumption testing ",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`server is up on port ${PORT}`);
