33f72b41955fe1dd0a2813150b4a5fd00f2eeefc
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-21 00:03:29 +0100

    saved admin authentication logics

---
+++ controller/admin/adminAuthController.ts
@@ -0,0 +1,43 @@
+import adminAuth from "../../model/admin/adminAuth";
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+
+export const adminSignup = async (req: Request, res: Response) => {
+  try {
+    const { companyName, companyEmail, yourName, password } = req.body;
+
+    const admin = await adminAuth.create({
+      companyName,
+      companyEmail,
+      yourName,
+      password,
+    });
+    return res.status(200).json({
+      message: "Success",
+      data: admin,
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      message: "an error occurred while creating admin",
+      data: error.message,
+    });
+  }
+};
+
+export const adminSignin = async (req: Request, res: Response) => {
+  try {
+    const { companyName, companyEmail, password } = req.body;
+
+    const admin = await adminAuth.findOne({ companyEmail });
+
+    return res.status(200).json({
+      message: "Success , admin is logged in",
+      data: admin,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "an error occurred while creating admin",
+      data: error.message,
+    });
+  }
+};
