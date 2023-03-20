c7613f27ba59f4eb857fbc4cbf98d4d2246ee5d2
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-20 23:25:20 +0100

    saved admin authentication model

---
+++ model/admin/adminAuth.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { adminSignUp } from "../allinterfaces";
+
+interface iAuth extends adminSignUp, mongoose.Document {}
+
+const AdminAuth = new mongoose.Schema<adminSignUp>({
+  companyName: {
+    type: String,
+    unique: true,
+    required: [true, "please enter your company name"],
+  },
+  companyEmail: {
+    type: String,
+    unique: true,
+    required: [true, "please enter your company email"],
+    trim: true,
+    lowercase: true,
+  },
+  yourName: {
+    type: String,
+    required: [true, "please enter your name"],
+  },
+  password: {
+    type: String,
+    required: [true, "please enter a password"],
+  },
+});
+
+const adminAuth = mongoose.model<iAuth>("adminAuthModel", AdminAuth);
+
+export default adminAuth;
