10fe7a55bb4968cd1661899d08b3b16a8690f854
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-21 15:55:11 +0100

    saved changes

--- controller/admin/adminAuthController.ts
+++ controller/admin/adminAuthController.ts
@@ -2,6 +2,8 @@ import adminAuth from "../../model/admin/adminAuth";
 import mongoose from "mongoose";
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import adminWalletModel from "../../model/admin/admindashboard/adminWallets";
+
 export const adminSignup = async (req: Request, res: Response) => {
   try {
     const { companyName, companyEmail, yourName, password } = req.body;
@@ -11,7 +13,7 @@ export const adminSignup = async (req: Request, res: Response) => {
     const dater = Date.now();
 
     const generateNumber = Math.floor(Math.random() * 78) + dater;
-    let num = 234;
+
     const admin = await adminAuth.create({
       companyName,
       companyEmail,
@@ -19,12 +21,18 @@ export const adminSignup = async (req: Request, res: Response) => {
       password: hash,
     });
 
-    /**	const createWallet = await WalletModel.create({
-			_id: regUser?._id,
-			Balance: 1000,
-			credit: 0,
-			debit: 0,
-		}); */
+    const createWallet = await adminWalletModel.create({
+      _id: admin?._id,
+      balance: 15000,
+      walletNumber: generateNumber,
+      credit: 0,
+      debit: 0,
+    });
+
+    admin?.wallet.push(new mongoose.Types.ObjectId(createWallet?._id));
+
+    admin.save();
+
     return res.status(200).json({
       message: "Success",
       data: admin,
@@ -39,7 +47,7 @@ export const adminSignup = async (req: Request, res: Response) => {
 
 export const adminSignin = async (req: Request, res: Response) => {
   try {
-    const { companyName, companyEmail, password } = req.body;
+    const { companyEmail, password } = req.body;
 
     const admin = await adminAuth.findOne({ companyEmail });
 
